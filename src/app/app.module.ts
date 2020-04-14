03f3cb5a576ddfbfc8501e76380679cf310b23d7
Marc <marcbaloran@hotmail.com>
2020-04-14 14:30:08 +0100

    Update shortcut button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { SearchInputComponent } from './main-content/search-input/search-input.component';
+import { AddShortcutComponent } from './main-content/add-shortcut/add-shortcut.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainContentComponent,
-    SearchInputComponent
+    SearchInputComponent,
+    AddShortcutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main-content/add-shortcut/add-shortcut.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-add-shortcut',
+  templateUrl: './add-shortcut.component.html',
+  styleUrls: ['./add-shortcut.component.css']
+})
+export class AddShortcutComponent implements OnInit {
+  // @Input() element: { URL: string, name: string, logo: string};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
